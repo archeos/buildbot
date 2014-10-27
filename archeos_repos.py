e0a3d3916c8e3a7bfb9b5923b38364e2246e6deb
Fabrizio Furnari <fab.furnari@gmail.com>
2014-10-27 13:58:22 +0100

    differentiate names

--- archeos_repos.py
+++ archeos_repos.py
@@ -1,8 +1,11 @@
 # These repos are checked in archeos_sources.py for modifications on ALL branches
-repos = {'archeos-manual': 'git://github.com/archeos/archeos-manual.git',
+all_branch_repos = {'archeos-manual': 'git://github.com/archeos/archeos-manual.git',
          'archeos-meta': 'git://github.com/archeos/archeos-meta.git',
          'archeos-desktop': 'git://github.com/archeos/archeos-desktop.git'
          }
 
 # These repos are checked in archeos_sources.py for modification only on branch master
 master_repos = {'cloudcompare-archeos': 'git://git@github.com:archeos/cloudcompare-archeos.git'}
+
+# this list must contain all repos
+all_repos = all_branch_repos + master_repos
