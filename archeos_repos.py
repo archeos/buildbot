000d4c600fcc43a38a4b8ab1e5462e47aef917b8
Fabrizio Furnari <fab.furnari@gmail.com>
2014-10-27 13:16:37 +0100

    added cloudcompare-archeos

--- archeos_repos.py
+++ archeos_repos.py
@@ -1,3 +1,8 @@
+# These repos are checked in archeos_sources.py for modifications on ALL branches
 repos = {'archeos-manuals': 'git://github.com/archeos/archeos-manuals.git',
          'archeos-meta': 'git://github.com/archeos/archeos-meta.git',
-         'archeos-desktop': 'git://github.com/archeos/archeos-desktop.git'}
+         'archeos-desktop': 'git://github.com/archeos/archeos-desktop.git'
+         }
+
+# These repos are checked in archeos_sources.py for modification only on branch master
+master_repos = {'cloudcompare-archeos': 'git://git@github.com:archeos/cloudcompare-archeos.git'}
--- archeos_sources.py
+++ archeos_sources.py
@@ -1,9 +1,8 @@
-from archeos_repos import repos
+from archeos_repos import repos, master_repos
 from buildbot.changes.gitpoller import GitPoller
 
 sources = []
 
-# At the moment only one type of source.
 # To add sources with other settings add other loops
 for name, url in repos.iteritems():
     sources.append(GitPoller(
@@ -12,3 +11,9 @@ for name, url in repos.iteritems():
         branches=True,
         pollinterval=300))
 
+for name, url in master_repos.iteritems():
+    sources.append(GitPoller(
+        url,
+        workdir='/home/builder/buildbot/gitpoller/' + name + '/',
+        branches=['master'],
+        pollinterval=300))
