5f66f12ba2700b9bfe04a3597d9d8f406614b05c
Fabrizio Furnari <fab.furnari@gmail.com>
2015-04-20 14:42:43 +0200

    added archeos-menu to buildbot

--- archeos_repos.py
+++ archeos_repos.py
@@ -3,7 +3,8 @@ all_branch_repos = {'archeos-manual': 'git://github.com/archeos/archeos-manual.g
          'archeos-meta': 'git://github.com/archeos/archeos-meta.git',
          'archeos-desktop': 'git://github.com/archeos/archeos-desktop.git',
          'test-repos': 'git://github.com/fabfurnari/test-repos.git',
-         'archeos-keyring': 'git://github.com/archeos/archeos-keyring.git'
+         'archeos-keyring': 'git://github.com/archeos/archeos-keyring.git',
+         'archeos-menu' : 'git://github.com/archeos/archeos-menu.git'
          }
 
 # These repos are checked in archeos_sources.py for modification only on branch master
